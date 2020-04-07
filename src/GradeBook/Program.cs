8e2af5dd9d9774cddff9bca43b900a5b98680394
Zhen <zhen.yang3@outlook.com>
2020-04-07 08:49:37 -0700

    CSharp Exercise
    using command Prompt create C# file

---
+++ src/GradeBook/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using GradeBook;
+
+namespace GradeBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+         Book book=new Book("Zhen's Gradebook");
+         book.AddGrade(89.1);
+         book.AddGrade(90.5);
+         book.AddGrade(77.5);
+
+         var stats=book.GetStatistics();   
+        Console.WriteLine( $"The lowest grade is {stats.Low}");
+        Console.WriteLine( $"The highest grade is {stats.High}");
+        Console.WriteLine( $"The average grade is {stats.Average:N1}");
+        }
+        
+    }
+}
